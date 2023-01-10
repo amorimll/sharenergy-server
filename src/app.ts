e98c3b414308236b4de5fbbc64af82cb163f2482
amorïm <63107417+amorimll@users.noreply.github.com>
2023-01-10 02:51:09 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -12,4 +12,4 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/auth", authRoutes);
-app.use("/", customerRoutes);
+app.use("/", verifyToken, customerRoutes);
