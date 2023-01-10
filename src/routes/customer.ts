377dd6148176179d4b7b9c7d376ec65039260bee
amorïm <63107417+amorimll@users.noreply.github.com>
2023-01-10 15:03:53 -0300

    Update customer.ts

--- src/routes/customer.ts
+++ src/routes/customer.ts
@@ -4,8 +4,8 @@ import { Request, Response } from "express";
 
 const router = express.Router();
 
-router.post("/customer", (req: Request, res: Response) => createCustomer(req, res));
-router.get("/customer", (req: Request, res: Response) => getAllCustomers(req, res));
-router.delete("/customer/:id", (req: Request, res: Response) => deleteCustomer(req, res));
-router.patch("/customer/:id", (req: Request, res: Response) => editCustomer(req, res));
+router.post("/", (req: Request, res: Response) => createCustomer(req, res));
+router.get("/", (req: Request, res: Response) => getAllCustomers(req, res));
+router.delete("/:id", (req: Request, res: Response) => deleteCustomer(req, res));
+router.patch("/:id", (req: Request, res: Response) => editCustomer(req, res));
 export default router;
